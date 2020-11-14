7528c5026e94a8e5deefeb6d451292e72e461f1d
Sutas Nakasawek <sutas@arterex.com>
2020-11-14 11:12:28 +0700

    add more translation in these languages: br portuguese, japanese, french, german (some), simplified chinese (some), vietnamese (some)

--- data/artifacts.js
+++ data/artifacts.js
@@ -1,4 +1,4 @@
-import { getFullnameInCurrentLanguage, LANGUAGE } from "../language.js";
+import { getFullnameInLanguage } from "../language.js";
 
 export const artifact_data = [
     {
@@ -6,7 +6,10 @@ export const artifact_data = [
         fullname: {
             en: "Adventurer",
             es: "Aventurero",
+            pt: "Aventureiro",
             ko: "모험가",
+            ja: "冒険者",
+            fr: "Aventurier",
             ru: "Искатель приключений",
         },
         item_list: [
@@ -24,7 +27,10 @@ export const artifact_data = [
         fullname: {
             en: "Archaic Petra",
             es: "Petra Arcaica",
+            pt: "Pedra Arcaica",
             ko: "유구한 반암",
+            ja: "悠久の磐岩",
+            fr: "Roche ancienne",
             ru: "Древний камень",
         },
         item_list: [
@@ -42,7 +48,10 @@ export const artifact_data = [
         fullname: {
             en: "Berserker (Blood-Soaked)",
             es: "Berserker",
+            pt: "Berserker",
             ko: "전쟁광",
+            ja: "狂戦士",
+            fr: "Berserker",
             ru: "Берсерк",
         },
         item_list: [
@@ -60,7 +69,10 @@ export const artifact_data = [
         fullname: {
             en: "Bloodstained Chivalry",
             es: "Caballería Sanguinaria",
+            pt: "Cavaleiro Manchado de Sangue",
             ko: "피에 물든 기사도",
+            ja: "血染めの騎士道",
+            fr: "Chevalerie ensanglantée",
             ru: "Рыцарь крови",
         },
         item_list: [
@@ -78,7 +90,10 @@ export const artifact_data = [
         fullname: {
             en: "Brave Heart",
             es: "Corazón del Guerrero",
+            pt: "Coração Valente",
             ko: "용사의 마음",
+            ja: "勇者の心",
+            fr: "Cœur du brave",
             ru: "Душа храбреца",
         },
         item_list: [
@@ -96,7 +111,10 @@ export const artifact_data = [
         fullname: {
             en: "Crimson Witch of Flames",
             es: "Bruja Carmesí en Llamas",
+            pt: "Bruxa das Chamas Carmesim",
             ko: "불타오르는 화염의 마녀",
+            ja: "燃え盛る炎の魔女",
+            fr: "Sorcière des flammes ardentes",
             ru: "Горящая алая ведьма",
         },
         item_list: [
@@ -114,7 +132,10 @@ export const artifact_data = [
         fullname: {
             en: "Defender's Will",
             es: "Corazón del Guardián",
+            pt: "Guardião",
             ko: "수호자의 마음",
+            ja: "守護の心",
+            fr: "Cœur du gardien",
             ru: "Дух защитника",
         },
         item_list: [
@@ -132,7 +153,10 @@ export const artifact_data = [
         fullname: {
             en: "Gambler",
             es: "Jugadora",
+            pt: "Apostador",
             ko: "노름꾼",
+            ja: "博徒",
+            fr: "Parieur",
             ru: "Азартный игрок",
         },
         item_list: [
@@ -165,7 +189,10 @@ export const artifact_data = [
         fullname: {
             en: "Gladiator's Finale",
             es: "Final del Gladiador",
+            pt: "Último Juramento do Gladiador",
             ko: "검투사의 피날레",
+            ja: "剣闘士のフィナーレ",
+            fr: "Rideau du gladiateur",
             ru: "Конец гладиатора",
         },
         item_list: [
@@ -197,7 +224,10 @@ export const artifact_data = [
         name: "instructor",
         fullname: {
             en: "Instructor",
+            pt: "Instrutor",
             ko: "교관",
+            ja: "教官",
+            fr: "Instructeur",
             ru: "Инструктор",
         },
         item_list: [
@@ -215,7 +245,10 @@ export const artifact_data = [
         fullname: {
             en: "Lavawalker",
             es: "Virtuoso Corredor de Lava",
+            pt: "Atravessador do Fogo",
             ko: "불 위를 걷는 현인",
+            ja: "烈火を渡る尊者",
+            fr: "Sage de la traverse du feu",
             ru: "Ступающий по лаве",
         },
         item_list: [
@@ -233,7 +266,10 @@ export const artifact_data = [
         fullname: {
             en: "Lucky Dog",
             es: "Afortunado",
+            pt: "O Sortudo",
             ko: "행운아",
+            ja: "幸運",
+            fr: "Chanceux",
             ru: "Везунчик",
         },
         item_list: [
@@ -251,7 +287,10 @@ export const artifact_data = [
         fullname: {
             en: "Maiden Beloved",
             es: "Doncella Amada",
+            pt: "Donzela Amada",
             ko: "사랑받는 소녀",
+            ja: "愛される少女",
+            fr: "Amour chéri",
             ru: "Возлюбленная юная дева",
         },
         item_list: [
@@ -269,7 +308,10 @@ export const artifact_data = [
         fullname: {
             en: "Martial Artist",
             es: "Artista Marcial",
+            pt: "Artista Marcial",
             ko: "무인",
+            ja: "武人",
+            fr: "Artiste martial",
             ru: "Воин",
         },
         item_list: [
@@ -287,7 +329,10 @@ export const artifact_data = [
         fullname: {
             en: "Noblesse Oblige",
             es: "Ritual Antiguo de la Nobleza",
+            pt: "Antigo Ritual Real",
             ko: "옛 왕실의 의식",
+            ja: "旧貴族のしつけ",
+            fr: "Ancien rituel royal",
             ru: "Церемония древней знати",
         },
         item_list: [
@@ -305,7 +350,10 @@ export const artifact_data = [
         fullname: {
             en: "Resolution of Sojourner",
             es: "Corazón del Viajero",
+            pt: "Resolução de Sojourner",
             ko: "여행자의 마음",
+            ja: "旅人の心",
+            fr: "Cœur du Guerrier",
             ru: "Дух путешествий",
         },
         item_list: [
@@ -323,7 +371,10 @@ export const artifact_data = [
         fullname: {
             en: "Retracing Bolide",
             es: "Retroceso del Meteorito",
+            pt: "Meteoro Invertido",
             ko: "날아오르는 유성",
+            ja: "逆飛びの流星",
+            fr: "Météore inversé",
             ru: "Встречная комета",
         },
         item_list: [
@@ -341,7 +392,10 @@ export const artifact_data = [
         fullname: {
             en: "Scholar",
             es: "Erudita",
+            pt: "O Estudioso",
             ko: "학사",
+            ja: "学者",
+            fr: "Erudit",
             ru: "Учёный",
         },
         item_list: [
@@ -359,7 +413,10 @@ export const artifact_data = [
         fullname: {
             en: "The Exile (Legacy)",
             es: "Exiliado",
+            pt: "O Exilado",
             ko: "유배자",
+            ja: "亡命者",
+            fr: "Exilé",
             ru: "Изгнанник",
         },
         item_list: [
@@ -377,7 +434,10 @@ export const artifact_data = [
         fullname: {
             en: "Thundering Fury",
             es: "Furia del Trueno",
+            pt: "Trovão Furioso",
             ko: "번개 같은 분노",
+            ja: "雷のような怒り",
+            fr: "Colère de tonnerre",
             ru: "Громогласный рёв ярости",
         },
         item_list: [
@@ -395,7 +455,10 @@ export const artifact_data = [
         fullname: {
             en: "Thundersoother",
             es: "Domador de Truenos",
+            pt: "Pacificador dos Trovões",
             ko: "뇌명을 평정한 존자",
+            ja: "雷を鎮める尊者",
+            fr: "Dompteur de tonnerre",
             ru: "Усмиряющий гром",
         },
         item_list: [
@@ -413,7 +476,10 @@ export const artifact_data = [
         fullname: {
             en: "Tiny Miracle",
             es: "Milagro",
+            pt: "Pequeno Milagre",
             ko: "기적",
+            ja: "奇跡",
+            fr: "Miracle",
             ru: "Маленькое чудо",
         },
         item_list: [
@@ -431,7 +497,10 @@ export const artifact_data = [
         fullname: {
             en: "Traveling Doctor (Curative)",
             es: "Médica Itinerante",
+            pt: "O Doutor Viajante (Curativo)",
             ko: "떠돌이 의사",
+            ja: "医者",
+            fr: "Médecin Itinérant",
             ru: "Целитель",
         },
         item_list: [
@@ -449,7 +518,10 @@ export const artifact_data = [
         fullname: {
             en: "Viridescent Venerer",
             es: "Sombra Verde Esmeralda",
+            pt: "Sombra Verde",
             ko: "청록색 그림자",
+            ja: "翆緑の影",
+            fr: "Ombre de la verte chasseuse",
             ru: "Изумрудная тень",
         },
         item_list: [
@@ -467,7 +539,10 @@ export const artifact_data = [
         fullname: {
             en: "Wanderer's Troupe",
             es: "Orquesta del Errante",
+            pt: "Grupo de Andarilhos",
             ko: "대지를 유랑하는 악단",
+            ja: "大地を流浪する楽団",
+            fr: "Bande vagabonde",
             ru: "Странствующий ансамбль",
         },
         item_list: [
@@ -486,7 +561,10 @@ export const artifact_data = [
         fullname: {
             en: "Prayers for Illumination",
             es: "Ritual de la Iluminación",
+            pt: "Xamã do Ritual Pyro",
             ko: "뇌명을 모시는 자",
+            ja: "火祭りの人",
+            fr: "Sacrifieur Pyro",
             ru: "Шаман огня",
         },
         item_list: [
@@ -504,7 +582,10 @@ export const artifact_data = [
         fullname: {
             en: "Prayers to Springtime",
             es: "Ritual de la Primavera",
+            pt: "Orações para a Primavera",
             ko: "얼음을 모시는 자",
+            ja: "氷祭りの人",
+            fr: "Sacrifieur Cryo",
             ru: "Шаман льда",
         },
         item_list: [
@@ -522,7 +603,10 @@ export const artifact_data = [
         fullname: {
             en: "Prayers for Wisdom",
             es: "Ritual de la Sabiduría",
+            pt: "Xamã do Ritual Electro",
             ko: "불을 모시는 자",
+            ja: "雷祭りの人",
+            fr: "Sacrifieur Electro",
             ru: "Шаман молнии",
         },
         item_list: [
@@ -540,7 +624,10 @@ export const artifact_data = [
         fullname: {
             en: "Prayers for Destiny",
             es: "Ritual del Destino",
+            pt: "Orações para o Destino",
             ko: "물을 모시는 자",
+            ja: "水祭りの人",
+            fr: "Sacrifieur Hydro",
             ru: "Шаман воды",
         },
         item_list: [
@@ -560,21 +647,20 @@ export const artifact_map = artifact_data.reduce(
     {}
 );
 
-export const artifact_option = artifact_data.map(artifact => ({
-    value: artifact.name,
-    label: getFullnameInCurrentLanguage(artifact),
-}));
-
 export const artifact_material = artifact_data.reduce(
     (acc, cur) => [
         ...acc,
         ...cur.item_list.map((titem, i) => ({
             name: `artifact_${cur.name}_${i}`,
-            fullname: {
-                [LANGUAGE]: `${getFullnameInCurrentLanguage(
-                    cur
-                )} ${getFullnameInCurrentLanguage(titem)}`,
-            },
+            fullname: Object.fromEntries(
+                Object.keys(cur.fullname).map(lang => [
+                    lang,
+                    `${getFullnameInLanguage(
+                        cur,
+                        lang
+                    )} ${getFullnameInLanguage(titem, lang)}`,
+                ])
+            ),
             farm_at: titem.farm_at,
             is_artifact: true,
             artifact: cur,
